febce7dcde16b398b95aa1b335fd76bd9990213e
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-09-25 01:30:59 -0300

    feature: implements save operation

--- src/main/java/br/com/amskywalker/graphql/adapters/in/web/UserController.java
+++ src/main/java/br/com/amskywalker/graphql/adapters/in/web/UserController.java
@@ -3,19 +3,30 @@ package br.com.amskywalker.graphql.adapters.in.web;
 import br.com.amskywalker.graphql.application.services.UserServiceImpl;
 import br.com.amskywalker.graphql.domain.model.User;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
 public class UserController {
-
     public UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService){
+        this.userService = userService;
+    }
 
     @QueryMapping
     public ResponseEntity<User> userById(@Argument UUID uuid){
         return ResponseEntity.ok(userService.getById(uuid));
+    }
+
+    @MutationMapping
+    public User createUser(@Argument String name, @Argument String birthDate, @Argument String motherName) {
+        User newUser = new User(UUID.randomUUID(), name, LocalDateTime.parse(birthDate), motherName);
+        return userService.createUser(newUser);
     }
 }
